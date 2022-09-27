4092a98df936092061838c7eeb44f95ce1f9eff2
b3nni <benjamin_nilsson@hotmail.com>
2022-09-27 15:26:44 +0200

    API skeleton + DB connection

---
+++ NZWalks/NZWalks.API/Model/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Model.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        // Navigation Properties
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
