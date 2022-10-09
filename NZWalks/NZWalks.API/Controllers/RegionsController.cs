cca99e9bf2e5bbc6554b1fd5a9251a914bf62eb8
b3nni <benjamin_nilsson@hotmail.com>
2022-10-09 14:57:41 +0200

    Regions Controller, DTO, Mapping, Async

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Model.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository _regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            _regionRepository = regionRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAllRegions()
+        {
+            IEnumerable<Region> regions = _regionRepository.GetAll();
+
+            // return DTO regions
+            var regionsDTO = new List<Model.DTO.Region>();
+            regions.ToList().ForEach(region =>
+            {
+                var regionDTO = new Model.DTO.Region()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    Area = region.Area,
+                    Lat = region.Lat,
+                    Long = region.Long,
+                    Population = region.Population
+                };
+
+                regionsDTO.Add(regionDTO);
+            });
+
+            return Ok(regionsDTO);
+        }
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Model.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext _nzWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext nzWalksDbContext)
+        {
+            _nzWalksDbContext = nzWalksDbContext;
+        }
+        public IEnumerable<Region> GetAll()
+        {
+            return _nzWalksDbContext.Regions.ToList();
+        }
+    }
+}
