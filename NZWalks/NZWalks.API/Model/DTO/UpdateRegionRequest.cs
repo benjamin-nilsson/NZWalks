b568cccb632f8e3921bd55224cc38012069e6c43
b3nni <benjamin_nilsson@hotmail.com>
2022-12-12 22:07:51 +0100

    Added Region Request models.

---
+++ NZWalks/NZWalks.API/Model/DTO/UpdateRegionRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Model.DTO
+{
+    public class UpdateRegionRequest
+    {
+        // Can remove some properties you don´t want the client to be able to update
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+    }
+}
